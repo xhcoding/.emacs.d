c91e33902be34c06f4f7c11e2abcd4ff51d62853
xhcoding <xhcoding@163.com>
2017-08-24 19:44:10 +0800

    using irony instead of ycmd;imporve init-cc.el

--- custom.el
+++ custom.el
@@ -3,13 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(package-selected-packages
-   (quote
-    (flycheck-plantuml zenburn-theme window-numbering web-mode uimage smartparens rainbow-delimiters popwin plantuml-mode php-mode ox-latex-chinese org-octopress monokai-theme hungry-delete ggtags flycheck-ycmd evil-visualstar evil-tutor evil-surround evil-leader evil-escape dired-sort dired+ counsel company-ycmd color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized))))
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -1,15 +1,57 @@
 ;; 需要的包
 
+;; .h作为cpp文件
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-
+;; 代码风格
+(setq c-default-style "linux")
 (setq c-basic-offset 4)
 
 
+;; 编译简单的c++文件
 
+(defun set-compile-command(command)
+  (set (make-local-variable 'compile-command) command))
 
+(defun my-compile-command()
+  (cond
+   ((file-exists-p "Makefile")
+    (setq command "make"))
+   ((file-exists-p "CMakeLists.txt")
+    (setq command "cmake -G \"Unix Makefiles\" ./"))
+   (t
+    (setq command (let ((file (file-name-nondirectory buffer-file-name)))
+		    (format "%s -o %s.out %s %s"
+			    "g++"
+			    (file-name-base file)
+			    "-g"
+			    file)))))
+  (set-compile-command command))
 
+(add-hook 'c-mode-common-hook 'my-compile-command)
+(require 'cc-mode)
 
+;; 运行
+(defun run-buffer-file()
+  (interactive)
+  (setq run-result-buffer "Result")
+  (setq file-directory (file-name-directory buffer-file-name))
+  (setq file-name (concat (file-name-base buffer-file-name) ".out"))
+  (setq file-absolute-path (concat file-directory file-name))
+  (when (file-exists-p file-absolute-path)
+    (if (get-buffer run-result-buffer)
+	(kill-buffer run-result-buffer))
+    (call-process-shell-command file-absolute-path nil (get-buffer-create run-result-buffer) nil)
+    (with-current-buffer run-result-buffer (help-mode))
+    (display-buffer run-result-buffer)))
+
+(defun compile-and-run()
+  (interactive)
+  (compile compile-command)
+  (run-buffer-file))
+
+(define-key c++-mode-map (kbd "<f7>") 'compile)
+(define-key c++-mode-map (kbd "<f8>") 'run-buffer-file)
+(define-key c++-mode-map (kbd "<f9>") 'compile-and-run)
 
 (provide 'init-cc)
