6cf7dba460f18cbf5ad20e9d194d6e289cf5171f
xhcoding <xhcoding@163.com>
2020-03-30 19:44:50 +0800

    feat: 增加补全

--- init.el
+++ init.el
@@ -307,4 +307,44 @@
   :commands (snails snails-search-point)
   :bind (("C-s" . snails)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                       补全                                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package company
+  :diminish
+  :hook (after-init . global-company-mode)
+  :commands company-cancel
+  :bind (("M-/" . company-complete)
+         ("C-M-i" . company-complete)
+         :map company-mode-map
+         ("<backtab>" . company-yasnippet)
+         :map company-active-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("<tab>" . company-complete-common-or-cycle)
+         ("<backtab>" . my-company-yasnippet)
+         :map company-search-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next))
+  :init
+  (setq company-idle-delay 0.2
+        company-tooltip-limit 10
+        company-minimum-prefix-length 2
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case nil
+        company-dabbrev-code-other-buffers t
+        company-tooltip-align-annotations t
+        company-require-match 'never
+        company-global-modes
+        '(not comint-mode erc-mode message-mode help-mode gud-mode)
+        company-frontends '(company-pseudo-tooltip-frontend company-echo-metadata-frontend)
+        company-backends '((:separate company-capf company-yasnippet))))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+
+
+
 ;;; init.el ends here
