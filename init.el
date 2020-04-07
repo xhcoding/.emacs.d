0598f43a563b3a6701d2a356e087d0d26fa314ec
xhcoding <xhcoding@163.com>
2020-04-07 13:22:45 +0800

    feat: 添加博客配置

--- init.el
+++ init.el
@@ -588,5 +588,77 @@
 (use-package groovy-mode
   :defer 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                   blog                                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(defconst +my-blog-root-dir
+  "~/Blog/"
+  "Blog root directory.")
+
+(defconst +my-blog-img-dir
+  (expand-file-name "images/" +my-blog-root-dir)
+  "Blog's image directory.")
+
+(use-package ox-hugo)
+
+(defun +my-blog*easy-hugo--org-headers (file)
+  "Return a draft org mode header string for a new article as FILE."
+  (let ((date
+         (format-time-string "%Y-%m-%d")))
+    (concat
+     "#+HUGO_BASE_DIR: ../"
+     "\n#+HUGO_SECTION: post"
+     "\n#+TITLE: " file
+     "\n#+DATE: " date
+     "\n#+AUTHOR:"
+     "\n#+HUGO_CUSTOM_FRONT_MATTER: :author \"xhcoding\""
+     "\n#+HUGO_TAGS: "
+     "\n#+HUGO_CATEGORIES: "
+     "\n#+HUGO_DRAFT: false"
+     "\n\n")))
+
+(use-package easy-hugo
+  :defer 1
+  :commands (easy-hugo)
+  :config
+  (setq easy-hugo-basedir (expand-file-name +my-blog-root-dir)
+        easy-hugo-postdir "blog"
+        easy-hugo-org-header t)
+  (evil-set-initial-state 'easy-hugo-mode 'emacs)
+  (advice-add #'easy-hugo--org-headers :override #'+my-blog*easy-hugo--org-headers)
+
+  (talon-leader-def
+    :keymaps 'normal
+    "ob" 'easy-hugo)
+  )
+
+(defun +my-blog/publish()
+  (interactive)
+  (let ((default-directory +my-blog-root-dir))
+    (call-process-shell-command "hugo")
+    (setq default-directory (expand-file-name "public" +my-blog-root-dir))
+    (call-process-shell-command "git add .")
+    (call-process-shell-command "git commit -m \"publish\"")
+    (call-process-shell-command "git push")
+    (message "publish finished")))
+
+(defun +my-blog/export-all()
+  (interactive)
+  (let ((default-directory (expand-file-name "blog" +my-blog-root-dir))
+        (files (directory-files (expand-file-name
+                                 "blog" +my-blog-root-dir))))
+    (seq-each
+     (lambda(file)
+       (when (and (not (string-equal "." file)) (not (string-equal ".." file)) (not (string-equal "config.toml" file)))
+         (with-temp-buffer
+           (find-file file)
+           (org-hugo-export-to-md))))
+     files)))
+
+
+
 (toggle-frame-fullscreen)
 ;;; init.el ends here
