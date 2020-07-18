bb13ccde15f5bf4f95ec1fd18fa497c99cd126f0
xhcoding <xhcoding@163.com>
2020-07-18 11:38:11 +0800

    fix: 更新 emacs 路径

--- etc/org_capture.bat
+++ etc/org_capture.bat
@@ -3,4 +3,4 @@ set URL=%1
 set URL=%URL:&=^&%
 set URL=%URL:/?=?%
 set URL=%URL:://=:///%
-start "" "C:\msys64\mingw64\bin\emacsclientw.exe"  -na "C:\msys64\mingw64\bin\runemacs.exe" "%URL%"
+start "" "C:\Program Files\Emacs\x86_64\bin\emacsclientw.exe"  -na "C:\Program Files\Emacs\x86_64\bin\runemacs.exe" "%URL%"
